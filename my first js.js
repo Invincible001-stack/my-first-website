a5e4f4c957c1498da53c9beeff720ce62bb3510f
Samuel <OKONS909@gmail.com>
2025-07-28 17:04:15 +0100

    first commit

---
+++ my first js.js
@@ -0,0 +1,22 @@
+document.title = "I am a great guy!!!"
+let name = window.prompt("What is your name?")
+console.log(`Hello ${name}`)
+console.warn("An error occured")
+console.error("The page has closed!")
+/*
+hello
+this
+is
+a
+multiline
+comment
+*/
+console.clear()
+name = "sammy"
+console.log(name)
+let tame = window.prompt("What is your surname?")
+console.log(tame)
+//window.close()
+
+const h1 = document.getElementById("goat")
+h1.innerText=`Hello ${name}`
